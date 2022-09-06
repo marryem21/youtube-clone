c4a0883febec698d0791590ecbb44583531eefd8
marryem21 <maryem-hamdi@student.kirkwood.edu>
2022-09-06 16:09:38 +0100

    Fisrt commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+const Name = styled.span`
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+const Date = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.textSoft};
+  margin-left: 5px;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+`;
+
+const Comment = () => {
+  return (
+    <Container>
+      <Avatar src="https://images.unsplash.com/photo-1500603720222-eb7a1f997356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" />
+      <Details>
+        <Name>
+          Samir Abdeslem <Date>1 day ago</Date>
+        </Name>
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
+          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
+          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
+          mollitia totam reiciendis?
+        </Text>
+      </Details>
+    </Container>
+  );
+};
+
+export default Comment;
---
+++ src/components/Comments.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+`;
+const Comments = () => {
+  return (
+    <Container>
+      <NewComment>
+        <Avatar src="https://images.unsplash.com/photo-1500603720222-eb7a1f997356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" />
+        <Input placeholder="Add a comment..." />
+      </NewComment>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+    </Container>
+  );
+};
+
+export default Comments;
